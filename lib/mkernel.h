315c39ca33963c7c716f20d858e3ab3be15b0e47
dnbh <dnb@cs.jhu.edu>
2017-04-12 13:27:47 -0400

    Remove commented section.

--- lib/mkernel.h
+++ lib/mkernel.h
@@ -9,35 +9,6 @@ using std::cout;
 
 namespace svm {
 
-#if 0
-template<typename FloatType>
-struct LinearKernel {
-    template<typename MatrixType1, typename MatrixType2>
-    MatrixType1 operator()(MatrixType1 &a, MatrixType2 &b) const {
-        MatrixType1 ret(a.rows(), a.columns());
-        for(size_t i(0), e(rows()), i != e; ++i) {
-            for(size_t i(0), e(rows()), i != e; ++i) {
-                return dot<MatrixType, FloatType>(a, b);
-            }
-        }
-    }
-};
-
-template<typename FloatType>
-struct RBFKernel {
-    // TODO: Expand this to somehow exploit matrix structure/instrinsics for better performance?
-    const FloatType mgamma_;
-    template<typename MatrixType>
-    FloatType operator()(MatrixType &a, MatrixType &b) const {
-        return std::exp(mgamma_ * diffnorm<MatrixType, FloatType>(a, b));
-    }
-    RBFKernel(FloatType gamma): mgamma_(-gamma) {}
-};
-#endif
-
-
-
-
 template<typename FloatType=float>
 struct TanhKernelMatrix {
     const FloatType k_;
