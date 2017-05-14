0091fc36e05ef42769ed2c7bd3b855116b7c0420
dnbh <dnb@cs.jhu.edu>
2017-05-14 16:40:15 -0400

    Add Laplacian kernel executable.

---
+++ src/train_laplace.cpp
@@ -0,0 +1,10 @@
+#include "src/train_kernel.h"
+
+#define LAPLACE_SWITCH case 'S': sigma = atof(optarg); break;
+#define KERNEL_PARAMS FLOAT_TYPE sigma(1.0);
+
+#define KERNEL_INIT LaplacianKernel<FLOAT_TYPE> kernel(sigma)
+DECLARE_KERNEL_SVM(KERNEL_INIT,\
+                   LAPLACE_SWITCH,\
+                   KERNEL_PARAMS,\
+                   "-S\tSigma for Laplace kernel [1.0] \n", "S:")
