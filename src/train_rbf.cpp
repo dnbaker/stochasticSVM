e472d00a08aa213b1d3e075b12280027a2d039a3
dnbh <dnb@cs.jhu.edu>
2017-05-14 14:01:15 -0400

    Consolidation.

--- src/train_rbf.cpp
+++ src/train_rbf.cpp
@@ -1,6 +1,5 @@
 #include "lib/kernel_svm.h"
 #include "src/run_svm.h"
-using namespace svm;
 
 int usage(char *ex) {
     char buf[1024];
@@ -17,22 +16,6 @@ int usage(char *ex) {
     cerr << buf;
     return EXIT_FAILURE;
 }
-
-class IntCounter {
-    std::map<int, int> map_;
-public:
-    void add(int val) {
-        ++map_[val];
-    }
-    std::string str() const {
-        std::string ret("{");
-        for(auto &pair: map_) ret += std::to_string(pair.first) + ": " + std::to_string(pair.second) + ", ";
-        ret.pop_back();
-        ret[ret.size() - 1] = '}';
-        return ret;
-    }
-};
-
 
 int main(int argc, char *argv[]) {
     int c, batch_size(256), nd_sparse(0);
