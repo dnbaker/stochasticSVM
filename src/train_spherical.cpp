847c8b83fcccade185f12f28eb63a765bb3d0c77
dnbh <dnb@cs.jhu.edu>
2017-05-14 16:54:18 -0400

    Spherical

---
+++ src/train_spherical.cpp
@@ -0,0 +1,10 @@
+#include "src/train_kernel.h"
+
+#define SPHERICAL_SWITCH case 'S': sigma = atof(optarg); break;
+#define KERNEL_PARAMS FLOAT_TYPE sigma(1.0);
+
+#define KERNEL_INIT SphericalKernel<FLOAT_TYPE> kernel(sigma)
+DECLARE_KERNEL_SVM(KERNEL_INIT,\
+                   SPHERICAL_SWITCH,\
+                   KERNEL_PARAMS,\
+                   "-S\tSigma for Spherical kernel [1.0] \n", "S:")
