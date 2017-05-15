657accf0fdb743d48e5e08d4628cc0952c0cc51e
dnbh <dnb@cs.jhu.edu>
2017-05-15 01:01:56 -0400

    Clean up metakernels.

--- lib/metakernel.h
+++ lib/metakernel.h
@@ -10,10 +10,12 @@ struct MultiplicativeKernel: KernelBase<FloatType> {
     const FloatType a_, b_;
     template<typename MatrixType1, typename MatrixType2>
     FloatType operator()(const MatrixType1 &a, const MatrixType2 &b) const {
-        return a_ * std::get<0>(kernels_)(a, b) * std::get<1>(kernels_)(a, b) + b_;
+        return a_ * kernels_.first(a, b) * kernels_(a, b) + b_;
     }
-    MultiplicativeKernel(std::pair<Kernel1, Kernel2> &kernels,
-                         FloatType a=1., FloatType b=1.): kernels_(std::move(kernels)), a_(a), b_(b) {}
+    MultiplicativeKernel(std::pair<Kernel1, Kernel2> kernels,
+                         FloatType a=1., FloatType b=1.): kernels_(kernels), a_(a), b_(b) {}
+    MultiplicativeKernel(Kernel1 k1, Kernel2 k2,
+                         FloatType a=1., FloatType b=1.): MultiplicativeKernel{std::make_pair(k1, k2), a, b} {}
     std::string str() const {
         return std::string("MultiplicativeKernel:[") + std::to_string(a_) +
             ", " + std::to_string(b_) + "]{" +
@@ -27,11 +29,11 @@ struct AdditiveKernel: KernelBase<FloatType> {
     const FloatType a_, b_, c_;
     template<typename MatrixType1, typename MatrixType2>
     FloatType operator()(const MatrixType1 &a, const MatrixType2 &b) const {
-        return std::get<0>(kernels_)(a, b) * a_ + std::get<1>(kernels_)(a, b) * b_;
+        return kernels_.first(a, b) * a_ +kernels_.second(a, b) * b_ + c_;
     }
-    AdditiveKernel(std::pair<Kernel1, Kernel2> &kernels,
+    AdditiveKernel(std::pair<Kernel1, Kernel2> kernels,
                    FloatType a=1., FloatType b=1., FloatType c=0.):
-        kernels_(std::move(kernels)), a_(a), b_(b), c_(c) {}
+        kernels_(kernels), a_(a), b_(b), c_(c) {}
     AdditiveKernel(Kernel1 kernel1, Kernel2 kernel2, FloatType a=1., FloatType b=1., FloatType c=0.):
         AdditiveKernel(make_pair(kernel1, kernel2), a, b, c) {}
     std::string str() const {
